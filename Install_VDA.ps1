0a1b1657bd76ab03c91b390912b7db72d05120b6
benjamin-crill <97719215+benjamin-crill@users.noreply.github.com>
2022-04-05 15:08:07 -0500

    Add files via upload

---
+++ Install_VDA.ps1
@@ -0,0 +1,34 @@
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)	
+
+$MyConfigFileloc = ("$env:Settings\Applications\Settings.xml")
+[xml]$MyConfigFile = (Get-Content $MyConfigFileLoc)
+
+$Vendor = "Citrix"
+$Product = "VDA"
+$Version = "2203" #$MyConfigFile.Settings.Citrix.Version
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogPath = "${env:SystemRoot}" + "\Temp\"
+
+$ListofDDCs = "icrill-ctxcc01.icrill.com cc01.icrill.com" #$MyConfigFile.Settings.Citrix.ListofDDCs
+#$ListofDDCs = $env:ListofDDCs
+
+$UnattendedArgs = '/noreboot /noresume /quiet /components vda /controllers "'+$ListofDDCs+'" /mastermcsimage /enable_remote_assistance /enable_hdx_ports /enable_hdx_udp_ports /disableexperiencemetrics /includeadditional "Citrix User Profile Manager WMI Plugin" /exclude "Personal vDisk","Citrix Telemetry Service" /virtualmachine /logpath "C:\Windows\Temp"'
+$Destination = "$Version\x64\XenDesktop Setup\"
+
+Start-Transcript $LogPS
+
+CD $Destination
+
+Write-Verbose "Starting Installation of $Vendor $Product $PackageName $Version" -Verbose
+(Start-Process -FilePath "XenDesktopVdaSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+write-Host $UnattendedArgs
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
