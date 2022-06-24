e8837f23b0135f2f1cb656633b131d4829a85b8b
benjamin-crill <97719215+benjamin-crill@users.noreply.github.com>
2022-06-24 13:54:59 -0500

    Add files via upload

--- InstallGoliath.ps1
+++ InstallGoliath.ps1
@@ -1,12 +1,4 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
-# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-# Example 3 EXE (Always use ' '):
-# $UnattendedArgs='/qn'
-# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-# Example 4 MSI (Always use " "):
-# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
-# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
 Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
@@ -16,15 +8,10 @@ $StartDTM = (Get-Date)
 $Vendor = "Goliath"
 $Product = "Performance Monitor"
 $Version = "12.0"
-$URL = $Evergreen.uri
-$InstallerType = "msi"
-$Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
-$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$UnattendedArgs = " InstallAgent.exe /S:SilentArgs.ini"
 $ProgressPreference = 'SilentlyContinue'
-$serveraddress = ""
+$serveraddress = "IP_OF_GOLIATH_SERVER"
 
 Start-Transcript $LogPS | Out-Null
   
